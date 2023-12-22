5e1ddd195b82d93ae1337e5927c0c9955ea9e449
callumhayden <callum.hayden@gmail.com>
2023-12-22 12:20:11 +0000

    added a new package, also set up the grid template for the main page, then also added the header and nav bar

--- GameBox/src/Components/App.jsx
+++ GameBox/src/Components/App.jsx
@@ -1,9 +1,31 @@
-import "./App.css";
+import "../styles/App.css";
+import { NavLink } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div></div>
+      <div className="container">
+        <header>
+          <h1 className="h1">GameBox</h1>
+          <section>
+            <nav>
+                <NavLink>HOME</NavLink>
+                <NavLink>PLAYSTATION</NavLink>
+                <NavLink>XBOX</NavLink>
+                <NavLink>CONSOLES</NavLink>
+                <NavLink>BOARD GAMES</NavLink>
+                <NavLink>ACCESSORIES</NavLink>
+            </nav>
+          </section>
+        </header>
+        <main>
+          <div className="left-menu"></div>
+          <div className="main-content"></div>
+        </main>
+        <footer>
+          <div className="footer-content"></div>
+        </footer>
+      </div>
     </>
   );
 }
