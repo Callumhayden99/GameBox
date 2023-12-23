3bac27461ad77e327efc9b79ab6aaf373df6a96c
callumhayden <callum.hayden@gmail.com>
2023-12-23 13:56:42 +0000

    working on the footer section.Geeting the layout complete then move on to breaking it down

--- GameBox/src/Components/App.jsx
+++ GameBox/src/Components/App.jsx
@@ -23,7 +23,42 @@ function App() {
           <div className="main-content"></div>
         </main>
         <footer>
-          <div className="footer-content"></div>
+          <h2 className="footer-header">Discover GameBox</h2>
+          <div className="products">Our Products
+          <ul>
+            <li>Playstation 5 Consoles</li>
+            <li>Xbox Consoles</li>
+            <li>Deals</li>
+            <li>Games</li>
+            <li>Accessories</li>
+            <li>Gift Cards</li>
+          </ul>
+          </div>
+          <div className="services">Our Services
+          <ul>
+            <li>Game Blog</li>
+            <li>Pre-order</li>
+            <li>Trade-in</li>
+            <li>Pre-owned guarantee</li>
+            <li>Store Finder</li>
+          </ul>
+          </div>
+          <div className="customer">Customer Service
+          <ul>
+            <li>Help Home</li>
+            <li>Contact Us</li>
+            <li>Deliveries</li>
+            <li>Returns Information</li>
+          </ul>
+          </div>
+          <div className="about">About
+          <ul>
+            <li>News</li>
+            <li>Privacy Centre</li>
+            <li>Terms & Conditions</li>
+            <li>Cookies</li>
+          </ul>
+          </div>
         </footer>
       </div>
     </>
